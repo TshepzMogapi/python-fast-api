a375e7239da3d986f495bc048c1589cd22b11b45
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-07-03 23:02:59 +0200

    create users endpoint

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
 from core.config import settings
 
+from apis.base import api_router
 from db.session import engine
 from db.base import Base
 
 def create_tables():
-	Base.metadata.create_all(bind=engine)
+  Base.metadata.create_all(bind=engine)
+
+def include_router(app):
+  app.include_router(api_router) 
 
 def start_application():
-	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
-	create_tables()
-	return app
+  app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+  include_router(app)
+  create_tables()
+  return app
 
 app = start_application()
--- src/schemas/user.py
+++ src/schemas/user.py
@@ -4,3 +4,11 @@ from pydantic import BaseModel, EmailStr
 class UserCreate(BaseModel):
   email: EmailStr
   password: str
+
+class ViewUser(BaseModel):
+  email: EmailStr
+  is_active: bool
+
+  class Config():
+    orm_mode = True
+  
